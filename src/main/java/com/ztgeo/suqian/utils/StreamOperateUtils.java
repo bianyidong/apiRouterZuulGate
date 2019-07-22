5e265b7ddecaa2a2d0e0821e9355447d5468205c
zhangerda <zhangerda@163.com>
2019-07-22 14:57:03 +0800

    增加限流过滤器（暂不支持定制过滤器）

--- src/main/java/com/ztgeo/suqian/utils/StreamOperateUtils.java
+++ src/main/java/com/ztgeo/suqian/utils/StreamOperateUtils.java
@@ -5,6 +5,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Objects;
+import java.util.UUID;
 
 /**
  * 流(inputStream、outputStream)相关工具集
@@ -68,5 +69,25 @@ public class StreamOperateUtils {
         }
     }
 
+    /**
+     *  生成不重复短UUID
+     * @return
+     */
+    public static String getShortUUID(){
+        char[] chars = new char[]{'a', 'b', 'c', 'd', 'e', 'f',
+                'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+                't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5',
+                '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+                'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+                'W', 'X', 'Y', 'Z'};
+        StringBuffer shortBuffer = new StringBuffer();
+        String uuid = UUID.randomUUID().toString().replace("-", "");
+        for (int i = 0; i < 8; i++) {
+            String str = uuid.substring(i * 4, i * 4 + 4);
+            int x = Integer.parseInt(str, 16);
+            shortBuffer.append(chars[x % 0x3E]);
+        }
+        return shortBuffer.toString();
+    }
 
 }
