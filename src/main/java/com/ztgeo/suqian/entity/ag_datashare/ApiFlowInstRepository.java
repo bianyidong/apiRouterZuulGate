c5a737876ac33785ba815afda760b40f9301cb4d
zhangerda <zhangerda@163.com>
2019-08-30 10:41:57 +0800

    1、限流过滤器细节优化

--- src/main/java/com/ztgeo/suqian/entity/ag_datashare/ApiFlowInstRepository.java
+++ src/main/java/com/ztgeo/suqian/entity/ag_datashare/ApiFlowInstRepository.java
@@ -11,6 +11,6 @@ public interface ApiFlowInstRepository extends CrudRepository<ApiFlowInst, Strin
 
     List<ApiFlowInst> findApiFlowInstsByApiIdEqualsAndIpEqualsOrderByEndTimeDesc(String apiId, String ip);
 
-    @Query(value = "select ifnull(sum(current_count),0) from api_flow_inst where start_time between ? and ?",nativeQuery = true)
-    int sumCurrentCountFromApiFlowInstsByStartTimeBetweenAnd(long stime, long etime);
+    @Query(value = "select ifnull(sum(current_count),0) from api_flow_inst where api_id = ? and start_time between ? and ?",nativeQuery = true)
+    int sumCurrentCountFromApiFlowInstsByStartTimeBetweenAnd(String apiId,long stime, long etime);
 }
