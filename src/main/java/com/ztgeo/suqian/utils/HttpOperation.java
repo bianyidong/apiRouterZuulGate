47cfc0761a42fef69496393a14e64e60bbb59889
zhangerda <zhangerda@163.com>
2019-09-23 10:50:39 +0800

    新建国家接口过滤器（请求与响应）

--- src/main/java/com/ztgeo/suqian/utils/HttpOperation.java
+++ src/main/java/com/ztgeo/suqian/utils/HttpOperation.java
@@ -1,8 +1,14 @@
 package com.ztgeo.suqian.utils;
 
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
 import com.ztgeo.suqian.common.ZtgeoBizRuntimeException;
+import com.ztgeo.suqian.filter.AddSendBodyFilter;
 import com.ztgeo.suqian.msg.CodeMsg;
 import okhttp3.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.TimeUnit;
 
@@ -13,6 +19,30 @@ import java.util.concurrent.TimeUnit;
  * @version 2018-12-13
  */
 public class HttpOperation {
+    private static Logger log = LoggerFactory.getLogger(HttpOperation.class);
+
+    public static String sendPostByApplicationXwwwFromUrlendcoded(String url,String params){
+        String respBody = null;
+        try {
+        HttpResponse<String> response = Unirest.post(url)
+                .header("content-type", "application/x-www-form-urlencoded")
+                .header("cache-control", "no-cache")
+                .body(params).asString();
+
+        int status = response.getStatus();
+        if(status == 200) {
+            respBody = response.getBody();
+        }else{
+            log.info("获取TOKEN失败，状态码：" + status);
+            throw new ZtgeoBizRuntimeException(CodeMsg.SDK_INTER_ERROR);
+        }
+
+        } catch (Exception e) {
+            log.info("调用sendPostByApplicationXwwwFromUrlendcoded方法异常！",e);
+            throw new ZtgeoBizRuntimeException(CodeMsg.SDK_INTER_ERROR);
+        }
+        return respBody;
+    }
 
 
     /**
