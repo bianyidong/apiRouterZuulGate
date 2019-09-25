95589071d4ab1f006a2665d64df4ca2765350df4
bianyidong <1056460744@qq.com>
2019-09-25 11:14:55 +0800

    1.测试

--- src/main/java/com/ztgeo/suqian/filter/ProvinceSharedReqFilter.java
+++ src/main/java/com/ztgeo/suqian/filter/ProvinceSharedReqFilter.java
@@ -69,7 +69,6 @@ public class ProvinceSharedReqFilter extends ZuulFilter {
                 return true;
         }
     }
-
     @Override
     public Object run() throws ZuulException {
 
@@ -87,12 +86,9 @@ public class ProvinceSharedReqFilter extends ZuulFilter {
             InputStream inReq = httpServletRequest.getInputStream();
             String requestBody = IOUtils.toString(inReq,Charset.forName("UTF-8"));
 
-
-
             // 重新配置请求体
             // 将JSON设置到请求体中，并设置请求方式为POST
             // BODY体设置
-
             final byte[] reqBodyBytes = requestBody.getBytes();
             requestContext.setRequest(new HttpServletRequestWrapper(httpServletRequest) {
 
